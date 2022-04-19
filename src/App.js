b62b968dcf2e649679515318b556ac99ba245b51
ankit.varia <ankit.varia@deskera.org>
2022-04-19 08:53:18 +0530

    form validation example

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Counter from './components/Counter';
 import Employee from './components/Employee';
 import Products from './components/Products';
+import { FormValidation } from './components/FormValidation';
 
 function App() {
   return (
     <div>
-      {/* <Counter />
-      <Employee /> */}
-      <Products />
+      {/* <Counter /> */}
+      {/* <Employee /> */}
+      {/* <Products /> */}
+      <FormValidation />
     </div>
   );
 }
